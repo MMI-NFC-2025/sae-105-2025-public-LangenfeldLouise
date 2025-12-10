2f83786563f22095471ed10a8c252ba35fd15ce0
Langenfeld Louise <louise.langenfeld@edu.univ-fcomte.fr>
2025-12-10 21:10:10 +0100

    css

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+
+// Largeur de défilement d’un item
+if (carousel) {
+    // Scroll au clic sur le bouton précédent
+    prevButton.addEventListener("click", () => {
+        const premierItem = document.querySelector(".carousel__item");
+        const scrollAmount = premierItem.clientWidth;
+        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    });
+
+    // Scroll au clic sur le bouton suivant
+    nextButton.addEventListener("click", () => {
+        const premierItem = document.querySelector(".carousel__item");
+        const scrollAmount = premierItem.clientWidth;
+        carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
+    });
+}
