f564e522777e60833814789c778546692007279e
Langenfeld Louise <louise.langenfeld@edu.univ-fcomte.fr>
2025-12-11 10:57:36 +0100

    lightbox

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,15 @@ if (carousel) {
         carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });
     });
 }
+/*lightbox*/
+const lightBox = document.querySelector("#lightbox");
+
+const lightBoxImg = document.querySelector("#lightbox img");
+
+lightBox?.addEventListener("click", () => lightBox.close());
+
+document.body.addEventListener("click", (evt) => {
+    if (!evt.target.matches("[data-full-img]")) return;
+    lightBoxImg.src = evt.target.dataset.fullImg;
+    lightBox.showModal();
+});
